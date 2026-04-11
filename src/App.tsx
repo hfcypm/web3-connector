432031cf0740b950346f4d57405f8313be65085d
lichaojie <hfcypm@126.com>
2026-04-11 15:20:46 +0800

    添加ethers库及window对象引用

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { ethers } from 'ethers'
 import './App.css'
 import WalletProvider from './wallet-sdk/provider'
 import { supportChainsConfigs } from './wallet-sdk/chain'
@@ -8,10 +8,20 @@ import type { Wallet } from './wallet-sdk/types';
 const defWallets: Wallet[] = [];
 
 function App() {
+  //window.ethereum：是 MetaMask 等钱包注入的原始对象，属于底层 API。
+  //ethers.BrowserProvider：是 ethers.js 库提供的包装类（Wrapper），它接收 window.ethereum 作为参数，然后创建出一个更高级的 provider 对象。
+  //钱包-ethers  provider
+  // window.ethereum  ──作为参数──>  new ethers.BrowserProvider()  ──返回──>  provider (ethers 实例)
+  //     (底层原始对象)                        (ethers 包装器)                    (高级 provider)
+  const provider = new ethers.BrowserProvider(window.ethereum)
   return (
     <>
-      <WalletProvider chains={supportChainsConfigs} wallets={defWallets}>
-        <h1 className='bg-red-400 h-20 flex items-center justify-center'>web3-connector TEST
+      <WalletProvider
+        chains={supportChainsConfigs}
+        provider={provider}
+        wallets={defWallets}
+        autoConnect={true}>
+        <h1 className='bg-red-400 h-20 flex items-center justify-center'>test
         </h1>
       </WalletProvider>
     </>
